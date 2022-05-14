ae72d42d005d000653ac1336da1f87d0775fbe05
fatihkrty <karatayy61@gmail.com>
2022-05-14 11:28:37 +0300

    ilk html dosyaları

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.listen(1453, function () {
+  console.log('Sunucu çalışıyor...');
+});
